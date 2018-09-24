062ff8b457f4a50ba0feca69c7ae5a44a92e01c4
rgreenblatt <hobo.ryang@gmail.com>
2018-09-24 17:41:05 -0400

    Testing resnet

--- scripts/train_net.py
+++ scripts/train_net.py
@@ -8,8 +8,10 @@ from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
 from keras.optimizers import SGD, Adagrad, Adadelta, Adam
 from keras.callbacks import TensorBoard, EarlyStopping
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
 from keras import initializers 
-from keras.layers import Activation, BatchNormalization
+from keras.layers import Activation, BatchNormalization, Input
 from keras import backend as K
 from keras.utils.generic_utils import get_custom_objects
 from keras.utils import multi_gpu_model
@@ -20,6 +22,8 @@ from hyperas.distributions import choice, uniform
 import sys
 from torus_transform_layer import torus_transform_layer
 from keras.models import load_model
+from keras.models import Model
+import keras
 
 
 #https://gist.github.com/williamFalcon/b03f17991374df99ab371eaeaa7ba610
@@ -35,104 +39,286 @@ def create_model(training_generator, testing_generator, length, num_gpus, weight
     
     get_custom_objects().update({'linear_bound_above_abs_1': Activation(linear_bound_above_abs_1)})
     get_custom_objects().update({'not_quite_linear': Activation(not_quite_linear)})
-    model = Sequential()
+    #model = Sequential()
 
     kernel_size_0 = {{choice([7, 9, 11])}}
 
-    model.add(torus_transform_layer((11,11),input_shape=(51,51,1)))
-    model.add(Convolution2D(16, (11, 11), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((11,11)))
-    model.add(Convolution2D(16, (11, 11), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((9,9)))
-    model.add(Convolution2D(16, (9, 9), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((9, 9)))
-    model.add(Convolution2D(16, (9, 9), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3, 3)))
-    #model.add(Convolution2D(16, (3, 3), strides=(2,2), activation=not_quite_linear))
-    model.add(MaxPooling2D((3,3), strides=(2,2)))
-
-    model.add(torus_transform_layer((7,7)))
-    model.add(Convolution2D(32, (7, 7), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((7,7)))
-    model.add(Convolution2D(32, (7, 7), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((5,5)))
-    model.add(Convolution2D(32, (5, 5), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((5,5)))
-    model.add(Convolution2D(32, (5, 5), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3, 3)))
-    #model.add(Convolution2D(32, (3, 3), strides=(2,2), activation=not_quite_linear))
-    model.add(MaxPooling2D((3,3), strides=(2,2)))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3, 3)))
-    #model.add(Convolution2D(32, (3, 3), strides=(2,2), activation=not_quite_linear))
-    model.add(MaxPooling2D((3,3), strides=(2,2)))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3,3)))
-    model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
-
-    model.add(torus_transform_layer((3, 3)))
-    #model.add(Convolution2D(32, (3, 3), strides=(2,2), activation=not_quite_linear))
-    model.add(MaxPooling2D((3,3), strides=(2,2)))
-
-    model.add(Flatten())
-
-    model.add(Dense(256, activation=not_quite_linear))
-    model.add(Dropout(0.4))
-
-    model.add(Dense(256, activation=not_quite_linear))
-    model.add(Dropout(0.4))
-
-    model.add(Dense(length, activation=linear_bound_above_abs_1))
     
-    sgd = SGD(lr=0.00005, decay=1e-6, momentum=0.7, nesterov=True)
+    def resnet_layer(inputs,
+                    num_filters=16,
+                    kernel_size=3,
+                    strides=1,
+                    activation='relu',
+                    batch_normalization=True,
+                    conv_first=True):
+        """2D Convolution-Batch Normalization-Activation stack builder
+    
+        # Arguments
+            inputs (tensor): input tensor from input image or previous layer
+            num_filters (int): Convolution2D number of filters
+            kernel_size (int): Convolution2D square kernel dimensions
+            strides (int): Convolution2D square stride dimensions
+            activation (string): activation name
+            batch_normalization (bool): whether to include batch normalization
+            conv_first (bool): conv-bn-activation (True) or
+                bn-activation-conv (False)
+    
+        # Returns
+            x (tensor): tensor as input to the next layer
+        """
+        conv = Convolution2D(num_filters,
+                      kernel_size=kernel_size,
+                      strides=strides,
+                      padding='valid',
+                      kernel_initializer='he_normal',
+                      kernel_regularizer=l2(1e-4))
+    
+        x = inputs
+        
+        if kernel_size != 1:
+            x  = torus_transform_layer((kernel_size, kernel_size))(x)
+        
+        if conv_first:
+            x = conv(x)
+            if batch_normalization:
+                x = BatchNormalization()(x)
+            if activation is not None:
+                x = Activation(activation)(x)
+        else:
+            if batch_normalization:
+                x = BatchNormalization()(x)
+            if activation is not None:
+                x = Activation(activation)(x)
+            x = conv(x)
+        return x
 
+    def resnet_v2(input_shape, depth, num_classes):
+        """ResNet Version 2 Model builder [b]
+    
+        Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Convolution2D or also known as
+        bottleneck layer
+        First shortcut connection per layer is 1 x 1 Convolution2D.
+        Second and onwards shortcut connection is identity.
+        At the beginning of each stage, the feature map size is halved (downsampled)
+        by a convolutional layer with strides=2, while the number of filter maps is
+        doubled. Within each stage, the layers have the same number filters and the
+        same filter map sizes.
+        Features maps sizes:
+        conv1  : 32x32,  16
+        stage 0: 32x32,  64
+        stage 1: 16x16, 128
+        stage 2:  8x8,  256
+    
+        # Arguments
+            input_shape (tensor): shape of input image tensor
+            depth (int): number of core convolutional layers
+            num_classes (int): number of classes (CIFAR10 has 10)
+    
+        # Returns
+            model (Model): Keras model instance
+        """
+        if (depth - 2) % 9 != 0:
+            raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+        # Start model definition.
+        num_filters_in = 16
+        num_res_blocks = int((depth - 2) / 9)
+    
+        inputs = Input(shape=input_shape)
+        # v2 performs Convolution2D with BN-ReLU on input before splitting into 2 paths
+        x = resnet_layer(inputs=inputs,
+                         num_filters=num_filters_in,
+                         kernel_size=11,
+                         conv_first=True)
+    
+        # Instantiate the stack of residual units
+        for stage in range(3):
+            for res_block in range(num_res_blocks):
+                this_kernal = 3
+                activation = not_quite_linear
+                batch_normalization = False
+                strides = 1
+                if stage == 0:
+                    this_kernal = 7
+                    num_filters_out = num_filters_in * 4
+                    if res_block == 0:  # first layer and first stage
+                        activation = None
+                        batch_normalization = False
+                else:
+                    num_filters_out = num_filters_in * 2
+                    if res_block == 0:  # first layer but not first stage
+                        strides = 2    # downsample
+    
+                # bottleneck residual unit
+                y = resnet_layer(inputs=x,
+                                 num_filters=num_filters_in,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=activation,
+                                 batch_normalization=batch_normalization,
+                                 conv_first=False)
+                y = resnet_layer(inputs=y,
+                                 num_filters=num_filters_in,
+                                 kernel_size=this_kernal,
+                                 conv_first=False)
+                y = resnet_layer(inputs=y,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 conv_first=False)
+                if res_block == 0:
+                    # linear projection residual shortcut connection to match
+                    # changed dims
+                    x = resnet_layer(inputs=x,
+                                     num_filters=num_filters_out,
+                                     kernel_size=1,
+                                     strides=strides,
+                                     activation=None,
+                                     batch_normalization=False)
+                    #x = resnet_layer(inputs=x,
+                    #                 num_filters=num_filters_out,
+                    #                 kernel_size=1,
+                    #                 strides=strides,
+                    #                 activation=None,
+                    #                 batch_normalization=False)
+                x = keras.layers.add([x, y])
+    
+            num_filters_in = num_filters_out
+    
+        # Add classifier on top.
+        # v2 has BN-ReLU before Pooling
+        x = Activation(not_quite_linear)(x)
+        x = AveragePooling2D(pool_size=8)(x)
+        y = Flatten()(x)
+        outputs = Dense(num_classes,
+                        activation=linear_bound_above_abs_1,
+                        kernel_initializer='he_normal')(y)
+    
+        # Instantiate model.
+        model = Model(inputs=inputs, outputs=outputs)
+        return model
+
+
+
+
+
+    #model.add(torus_transform_layer((11,11),input_shape=(51,51,1)))
+    #model.add(Convolution2D(16, (11, 11), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((11,11)))
+    #model.add(Convolution2D(16, (11, 11), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((9,9)))
+    #model.add(Convolution2D(16, (9, 9), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((9, 9)))
+    #model.add(Convolution2D(16, (9, 9), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3, 3)))
+    #model.add(MaxPooling2D((3,3), strides=(2,2)))
+
+    #model.add(torus_transform_layer((7,7)))
+    #model.add(Convolution2D(32, (7, 7), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((7,7)))
+    #model.add(Convolution2D(32, (7, 7), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((5,5)))
+    #model.add(Convolution2D(32, (5, 5), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((5,5)))
+    #model.add(Convolution2D(32, (5, 5), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3, 3)))
+    #model.add(MaxPooling2D((3,3), strides=(2,2)))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(64, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3, 3)))
+    #model.add(MaxPooling2D((3,3), strides=(2,2)))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3,3)))
+    #model.add(Convolution2D(128, (3, 3), activation=not_quite_linear))
+
+    #model.add(torus_transform_layer((3, 3)))
+    #model.add(MaxPooling2D((3,3), strides=(2,2)))
+
+    #model.add(Flatten())
+
+    #model.add(Dense(256, activation=not_quite_linear))
+    #model.add(Dropout(0.4))
+
+    #model.add(Dense(256, activation=not_quite_linear))
+    #model.add(Dropout(0.4))
+
+    #model.add(Dense(length, activation=linear_bound_above_abs_1))
+   
+    def lr_schedule(epoch):
+        """Learning Rate Schedule
+
+        Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+        Called automatically every epoch as part of callbacks during training.
+
+        # Arguments
+            epoch (int): The number of epochs
+
+        # Returns
+            lr (float32): learning rate
+        """
+        lr = 1e-3
+        if epoch > 180:
+            lr *= 0.5e-3
+        elif epoch > 160:
+            lr *= 1e-3
+        elif epoch > 120:
+            lr *= 1e-2
+        elif epoch > 80:
+            lr *= 1e-1
+        print('Learning rate: ', lr)
+        return lr
+
+    n = 3
+    depth = n * 9 + 2
+
+
+    model = resnet_v2((51,51,1), depth, length)
+
+ 
     if num_gpus > 1:
         model = multi_gpu_model(model, gpus=num_gpus) 
 
@@ -142,9 +328,18 @@ def create_model(training_generator, testing_generator, length, num_gpus, weight
     if save_path != None:
         model = load_model(save_path)
     
-    model.compile(optimizer=Adam(lr=0.0001, amsgrad=use_amsgrad), loss='mean_squared_error')
+    model.compile(optimizer=Adam(lr=lr_schedule(0)), loss='mean_squared_error', metrics=['accuracy'])
     
-    earlyStopping=EarlyStopping(monitor='val_loss', patience=8, verbose=0, mode='auto', min_delta=0.007)
+    #earlyStopping=EarlyStopping(monitor='val_loss', patience=8, verbose=0, mode='auto', min_delta=0.007)
+    
+    lr_scheduler = LearningRateScheduler(lr_schedule)
+
+    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                                   cooldown=0,
+                                   patience=5,
+                                   min_lr=0.5e-6)
+
+    callbacks = [lr_reducer, lr_scheduler]
 
     #tbCallBack = TensorBoard(log_dir='./graph', write_graph=True, write_images=True)
     
@@ -153,23 +348,11 @@ def create_model(training_generator, testing_generator, length, num_gpus, weight
                     use_multiprocessing=True,
                     workers=8,
                     epochs=80,
-                    callbacks=[earlyStopping]
+                    callbacks=callbacks
                     )
     
     model.save('model_initial.h5')
     
-    model.compile(optimizer=sgd, loss='mean_squared_error')
-
-    model.fit_generator(generator=training_generator,
-                    validation_data=testing_generator,
-                    use_multiprocessing=True,
-                    workers=8,
-                    epochs=80,
-                    callbacks=[earlyStopping]
-                    )
-
-    model.save('model_final.h5')
-
     acc = model.evaluate_generator(generator=testing_generator,
                     use_multiprocessing=True,
                     workers=8)
